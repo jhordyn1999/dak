094094134b8f31d1d1e7fe1f1c1bb121bedb1109
Jhordyn1999 <jhordynalexander10@gmail.com>
2022-07-06 22:43:10 -0500

    cierre daktico

--- js/plataforma.js
+++ js/plataforma.js
@@ -8,6 +8,34 @@ var app = new Vue({
     },
     methods: {
        
+        rutaFormulario(){
+            window.location.href = "../formulario.html";
+        },
+        changueMenu() {
+            const btnMenu = document.getElementById("btnMenu");
+            const btnSalir = document.getElementById("btnSalir");
+            const divMenu = document.getElementById("divMenu");
 
+
+            if (divMenu.style.display = "none") {
+                divMenu.style.display = "block"
+                btnSalir.style.display = "block"
+                btnMenu.style.display = "none"
+
+            }
+        },
+        changueMenuSalir() {
+            const btnMenu = document.getElementById("btnMenu");
+            const btnSalir = document.getElementById("btnSalir");
+            const divMenu = document.getElementById("divMenu");
+
+
+            if (divMenu.style.display = "block") {
+                divMenu.style.display = "none"
+                btnSalir.style.display = "none"
+                btnMenu.style.display = "block"
+
+            }
+        },
     },
 });
